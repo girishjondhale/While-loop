46d2aa972bb922a4004ec4a7699169afcc6201d9
girishjondhale <girishjondhale1997@gmail.com>
2023-08-16 17:12:12 -0700

    Add project files.

---
+++ While loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_loop
+{
+    internal class assignment_1
+    {//Print the value from 1 to 50
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 50)
+            {
+                Console.WriteLine(i);
+
+                i++;
+            }
+
+        }
+    }
+}
